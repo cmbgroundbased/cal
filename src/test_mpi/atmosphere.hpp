a1c4095bc730b21fff4fb775e391dbb5d8946c60
Stefano Mandelli <linuxfree2@gmail.com>
2020-07-13 21:09:26 +0200

    Update cookbook file

--- src/test_mpi/atmosphere.hpp
+++ src/test_mpi/atmosphere.hpp
@@ -9,45 +9,46 @@
 #ifndef ATM_TEST_HPP
 #define ATM_TEST_HPP
 
-double azmin = 0.0;
-double azmax = M_PI;
-double elmin = 0;
-double elmax = M_PI/2;
+double azmin = (0/180)*M_PI;
+double azmax = (359/180)*M_PI;
+double elmin = (60/180)*M_PI;
+double elmax = (80/180)*M_PI;
 
 double tmin = 0;
-double tmax = 3600;
+double tmax_sim = 3600.0;
+double tmax_tod = tmax_sim/2;
 
-double lmin_center = 10.0;
+double lmin_center = 0.01;
 double lmin_sigma = 0.001;
 
-double lmax_center = 300;
+double lmax_center = 30;
 double lmax_sigma = 10;
 
 double w_center = 2;
-double w_sigma = 0.2;
+double w_sigma = 0;
 
-double wdir_center = 10;
-double wdir_sigma = 0.5;
+double wdir_center = -1.78;
+double wdir_sigma = 0;
 
 double z0_center = 2000;
 double z0_sigma = 0;
 
 double T0_center = 280;
-double T0_sigma = 10;
+double T0_sigma = 0;
 
 double zatm = 40000;
 double zmax = 2000;
 
-double xstep = 100;
-double ystep = 100;
-double zstep = 100;
+double xstep = 20;
+double ystep = 20;
+double zstep = 20;
 
-long nelem_sim_max = 200;
+long nelem_sim_max = 100000;
 
 int verbosity = 1;
 
 uint64_t key1 = 0;
-uint64_t key2 = 10;
+uint64_t key2 = 2<<32;
 uint64_t counterval1 = 0;
 uint64_t counterval2 = 0;
 std::string cachedir = std::string("/tmp/");
