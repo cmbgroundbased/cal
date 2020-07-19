e06a92d2da514040550383a650c9db83c48beb3b
Stefano Mandelli <linuxfree2@gmail.com>
2020-07-19 11:42:44 +0200

    Remeved junks files

--- build/src/libcal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/algebrato/Progetti/CMB4G/libcal/src/libcal
-# Build directory: /home/algebrato/Progetti/CMB4G/libcal/build/src/libcal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/algebrato/Progetti/CMB4G/libcal/src/test
-# Build directory: /home/algebrato/Progetti/CMB4G/libcal/build/src/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
