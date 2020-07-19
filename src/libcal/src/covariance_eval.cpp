94afd86b0f8507347cc787275eeee7fcd2eacf5e
Stefano Mandelli <linuxfree2@gmail.com>
2020-07-19 11:41:28 +0200

    On branch nonMPI_wrappers  Changes to be committed: 	new file:   libcal/CMakeLists.txt 	new file:   libcal/include/AATM_fun.hpp 	new file:   libcal/include/CALAtmSim.hpp 	new file:   libcal/include/Random123/MicroURNG.hpp 	new file:   libcal/include/Random123/ReinterpretCtr.hpp 	[etc..]

---
+++ src/libcal/src/covariance_eval.cpp
@@ -0,0 +1,67 @@
+/*
+   Copyright (c) 2015-2018 by the parties listed in the AUTHORS file.
+   All rights reserved.  Use of this source code is governed by
+   a BSD-style license that can be found in the LICENSE file.
+ */
+
+#include <CALAtmSim.hpp>
+
+double cal::atm_sim::cov_eval(double * coord1, double * coord2)
+{
+    // Evaluate the atmospheric absorption covariance between two coordinates
+    // Church (1995) Eq.(6) & (9)
+    // Coordinates are in the horizontal frame
+
+    const long nn = 1;
+
+    // Uncomment these lines for smoothing
+    // const double ndxinv = xxstep / (nn-1);
+    // const double ndzinv = zzstep / (nn-1);
+    const double ninv = 1.; // / (nn * nn);
+
+    double val = 0;
+
+    for (int ii1 = 0; ii1 < nn; ++ii1) {
+        double xx1 = coord1[0];
+        double yy1 = coord1[1];
+        double zz1 = coord1[2];
+
+        // Uncomment these lines for smoothing
+        // if ( ii1 ) {
+        //    xx1 += ii1 * ndxinv;
+        //    zz1 += ii1 * ndzinv;
+        // }
+
+        for (int ii2 = 0; ii2 < nn; ++ii2) {
+            double xx2 = coord2[0];
+            double yy2 = coord2[1];
+            double zz2 = coord2[2];
+
+            // Uncomment these lines for smoothing
+            // if ( ii2 ) {
+            //    xx2 += ii2 * ndxinv;
+            //    zz2 += ii2 * ndzinv;
+            // }
+
+            double dx = xx1 - xx2;
+            double dy = yy1 - yy2;
+            double dz = zz1 - zz2;
+            double r2 = dx * dx + dy * dy + dz * dz;
+            if (r2 < rcorrsq) {
+                double r = sqrt(r2);
+
+                // Water vapor altitude factor
+
+                double chi1 = std::exp(-(zz1 + zz2) * z0inv);
+
+                // Kolmogorov factor
+
+                double chi2 = kolmogorov(r);
+
+                val += chi1 * chi2;
+            }
+        }
+    }
+
+    return val * ninv;
+}
---
+++ src/libcal/src/simulation.cpp
@@ -0,0 +1,64 @@
+/*
+   Copyright (c) 2015-2018 by the parties listed in the AUTHORS file.
+   All rights reserved.  Use of this source code is governed by
+   a BSD-style license that can be found in the LICENSE file.
+ */
+
+#include <CALAtmSim.hpp>
+
+/** Simulate the atmosphere in indipendent slices, each slice is assigned at one process. */
+int cal::atm_sim::simulate(bool use_cache)
+{
+    if (use_cache) load_realization();
+    if (cached) return 0;
+
+    try {
+        draw();
+        get_volume();
+        compress_volume();
+        try {
+            realization.reset(new AlignedVector <double> (nelem));
+            std:fill(realization->begin(), realization->end(), 0.0);
+        } catch (...) {
+            std::cerr << rank << " : Allocation failed. nelem = " << nelem << std::endl;
+            throw;
+        }
+        cal::Timer tm;
+        tm.start();
+
+        uint64_t ind_start = 0, ind_stop = 0, slice = 0;
+
+        // Simulate the atmosphere in indipendent slices, each slice is assigned at one process.
+
+        std::vector <int> slice_starts;
+        std::vector <int> slice_stops;
+
+        while(true) {
+            get_slice(ind_start, ind_stop);
+            slice_starts.push_back(ind_start);
+            slice_stops.push_back(ind_stop);
+
+            if (slice % ntask == rank) {
+                cholmod_sparse * cov = build_sparse_covariance(ind_start, ind_stop);
+                cholmod_sparse * sqrt_cov = sqrt_sparse_covariance(cov, ind_start, ind_stop);
+                cholmod_free_sparse(&cov, chcommon);
+                apply_sparse_covariance(sqrt_cov,
+                                        ind_start,
+                                        ind_stop);
+                cholmod_free_sparse(&sqrt_cov, chcommon);
+            }
+            counter2 += ind_stop - ind_start;
+
+            if (ind_stop == nelem) break;
+            ++slice;
+        }
+        // smooth();?
+        tm.stop();
+    } catch (const std::exception & e) {
+        std::cerr << "ERROR: atm::simulate failed with: " << e.what() << std::endl;
+    }
+    cached = true;
+    if (use_cache) save_realization();
+
+    return 0;
+}
